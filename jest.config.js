d7216662973357a13a9cb56b1d0cae2a156d0e1a
Subham Mishra <mba21081998@gmail.com>
2025-05-15 17:18:42 +0530

    Feat/Add unit tests for SideBar component to cover key UI and interaction behaviors (#17)

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,14 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
-module.exports = {
-  testEnvironment: 'node',
-  transform: {
-    '^.+\.tsx?$': ['ts-jest', {}],
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({ dir: './' });
+
+const customJestConfig = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/components/(.*)$': '<rootDir>/components/$1',
+    '^@/app/(.*)$': '<rootDir>/app/$1',
   },
 };
+
+module.exports = createJestConfig(customJestConfig);
